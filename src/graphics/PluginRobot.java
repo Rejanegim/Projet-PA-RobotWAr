f6bbe025a8fdd16bd50b7dc94d94009397e8e35b
rihabbh <rihab.benh@gmail.com>
2016-11-10 10:19:26 +0100

    maj 2

--- src/graphics/PluginRobot.java
+++ src/graphics/PluginRobot.java
@@ -2,54 +2,61 @@ package m1.miage.projet.pa;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import javax.swing.JPanel;
-
 import java.lang.Math;
 
 
-public class PluginRobot extends JPanel implements IPluginGraphique {
+
+public class PluginRobot  implements IPluginGraphique {
+	
+	
+	private static final long serialVersionUID = 1L;
+	
 	
 	float  randomr =(float) (Math.random()); // opacite de la couleur rouge
 	float  randomv =(float) (Math.random()); // opacite de la couleur vert
 	float  randomb =(float) (Math.random()); // opacite de la couleur bleu
-	int randomx= (int) (Math.random() * xMax) ;
-	int randomy= (int) (Math.random() * yMax ) ;
+	int x= (int) (Math.random() * xMax) ;
+	int y= (int) (Math.random() * yMax ) ;
 
 
 	public static final int xMax = 500; // abscisse maximun de la fenetre
 	public static final int yMax = 500; // ordonnee maximun de la fenetre
-	// l'origine (i.e. (0,0)) represente le coin superieur gauche de la fenetre
-	// instance variables
-	private float x; // abcisse du centre du carre
-	private float y; // ordonnee du centre du carre
-
+	
 	public PluginRobot() {
+		super();
+		this.randomr = randomr;
+		this.randomv = randomv;
+		this.randomb = randomb;
 		this.x = x;
+		this.y =y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
 		this.y = y;
 	}
 
-	
-	public void setX(float x) {
-		this.x = x;
-	}
-
-	public void setY(float y) {
-		this.y = y;
-	}
-	
-	
-	
 	@Override
 	public void paint(Graphics g) {
-		System.out.println(randomr +" " +randomb +" " +randomv);
 		Color c = new Color(randomr,randomv,randomb);
 		g.setColor(c);
-		System.out.println(randomx   +" " +randomy); // affiche les coordonnés
-		g.drawRect(randomx, randomy, 10, 10);
-		g.fillRect(randomx, randomy, 10, 10);
+		g.drawRect(x, y, 10, 10);
+		g.fillRect(x, y, 10, 10);
 		
 	}
 
 
 
 }
+
