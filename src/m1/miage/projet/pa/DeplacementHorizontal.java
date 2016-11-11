af7fa563a53c5188a09d67bb7253fd33d66786b8
rihabbh <rihab.benh@gmail.com>
2016-11-11 22:47:08 +0100

    Deplacement
    j'ai réglé le problème des repaints mais ces classes restent à améliorer.

---
+++ src/m1/miage/projet/pa/DeplacementHorizontal.java
@@ -0,0 +1,68 @@
+package m1.miage.projet.pa;
+
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import m1.miage.projet.pa.Fenetre;
+import m1.miage.projet.pa.PluginRobot;
+
+
+public class DeplacementHorizontal {
+
+	private ArrayList<PluginRobot> listRobots;
+
+	private Fenetre fenetre;
+
+	public DeplacementHorizontal(int nbRobots) {
+
+		fenetre = new Fenetre();
+
+		listRobots = new ArrayList();
+
+		// Création des robots :
+		for (int i = 0; i < nbRobots; i++) {
+			PluginRobot r1 = new PluginRobot();
+
+			listRobots.add(r1);
+		}
+
+		fenetre.setListeRobots(listRobots);
+
+		gestionDesTours();
+
+	}
+
+	private void gestionDesTours() {
+System.out.println("TOURS");
+		// 10 tours de jeu :
+		for (int i = 1; i <= 10; i++) {
+
+			for (PluginRobot robot : listRobots) {
+				try {
+					TimeUnit.SECONDS.sleep(1);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				int x = robot.getX();
+				int y = robot.getY();
+
+				robot.setX(x + 1);
+				
+				
+				System.out.println("mouvement");
+				
+				fenetre.repaint();
+			}
+
+		}
+
+	}
+
+	public static void main(String[] args) {
+		DeplacementHorizontal moteur = new DeplacementHorizontal(2);
+		
+	}
+}
