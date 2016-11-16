432744e23818840b029f54a5c1884285b81f2b6d
rihabbh <rihab.benh@gmail.com>
2016-11-16 21:53:31 +0100

    MyClassLoader

---
+++ Moteur/src/main/java/unice/miage/m1/projet/MyClassLoader.java
@@ -0,0 +1,54 @@
+package m1.miage.projet.pa;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.security.SecureClassLoader;
+import java.util.ArrayList;
+
+
+	public class MyClassLoader extends SecureClassLoader {
+		private ArrayList<File> path = new ArrayList<File>();
+
+		@Override
+		protected Class<?> findClass(String name) throws ClassNotFoundException {
+			byte[] b = null;
+			try {
+				b = loadClassData(name);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return super.defineClass(name, b, 0, b.length);
+		}
+
+		private byte[] loadClassData(String name) throws ClassNotFoundException, IOException {
+			// TODO A COMPLETER
+			String nameFile = name.replace(".", File.separator);
+			nameFile = nameFile + ".class";
+			byte[] byt = null;
+			for (int i = 0; i < path.size(); i++) {
+				if (path.get(i).isDirectory()) {
+					File fi = new File(path.get(i), nameFile);// obtenir le chemin//
+																// absolue
+					byt = Files.readAllBytes(fi.toPath());
+				} 
+			}
+	
+			return byt;
+		}
+
+		public ArrayList<File> getPath() {
+			return path;
+		}
+
+		public void setPath(File path) {
+			this.path.add(path) ;
+		}
+
+		public static void main(String[] args) {
+			
+		}
+}
